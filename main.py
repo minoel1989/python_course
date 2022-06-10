7c6002fb2be0fd8d3dd620b5ad574b1d0145f70a
elo\minoel <minoel.pendi@gmail.com>
2022-06-10 21:29:05 +0200

    new *

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-# This is a sample Python script.
+# # This is a sample Python script.
+#
+# # Press Shift+F10 to execute it or replace it with your code.
+# # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#
+#
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('Minoel')
+#
+#
+# print_hi('elo')
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+mylist = [1,2,3,4,5,6,7,8,9]
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Minoel')
-
-
-print_hi('elo')
+for num in mylist:
+    if num % 2 == 0:
+        print(f'Even Number: {num}')
+    else:
+        print(f'Odd Number: {num}')
